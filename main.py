c2183cb3e402e69bbffd73c5fc35cdf013f2251e
Artyom Kosakyan <artyom.kosakyan@sparkers.com>
2023-07-10 18:57:43 +0400

    first function with docstrings

---
+++ main.py
@@ -0,0 +1,10 @@
+###############################
+# docstrings and annotations #
+###############################
+
+def first_example_of_docstring_usage():
+    """
+    First example of usage docstrings
+    :return: None
+    """
+    return None
